27890f2f8b43b9e1599b5bf9bea4f4d3dfe0c835
amdia <15892716+amdia@users.noreply.github.com>
2018-10-15 11:21:25 +0200

    correct userdata input state

--- src/guiding_as/guiding_action_server.py
+++ src/guiding_as/guiding_action_server.py
@@ -1137,7 +1137,7 @@ class AskShowPlace(smach.State):
         """
         rospy.loginfo("Initialization of " + self.get_name() + " state")
         smach.State.__init__(self, outcomes=['get_answer', 'show', 'not_show', 'preempted'],
-                             input_keys=['goal_frame', 'human_look_at_point'],
+                             input_keys=['target_frame', 'human_look_at_point'],
                              output_keys=['question_asked'])
 
     def get_name(self):
